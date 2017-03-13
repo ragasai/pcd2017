8f237430c435601f60c7db1d0b116fa0192f5c06
Your Name <you@example.com>
2017-03-13 09:17:16 +0530

    equation

---
+++ week3/roots.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<math.h>
+struct complex
+{
+float real;
+float imaginary;
+};
+struct roots
+{
+struct complex root 1;
+struct complex root 2;
+};
+int findroots(float a,float b, float c,struct complex *x,struct complex 
+*y)
+{
+if(a==0)
+{
+return -1;
+}
+float d=b*b-4*a*c;
+if(d==0)
+{
+x->real=-b/(2*a);
+x->imaginary=0;
+y->real=-b/(2*a);
+y->imaginary=0;
+return 0;
+}
+if(d>0)
+{
+x->real=(-b+sqrt(d))/(2*a);
+x->imaginary=0;
+y->real=(-b-sqrt(d))/(2*a);
+y->imaginary=0;
+return 1;
+}
+if(d<0)
+{
+x->real=-b/(2*a);
+x->imaginary=sqrt(fabs(d))/(2*a);
+y->real=-b/(2*a);
+y->imaginary=sqrt(fabs(d))/(2*a);
+return 2;
+}
+return -1;
+}
+int main()
+{
+float real, imaginary,disc;
+int d;
+struct complex r1,r2;
+d=findroots(1.0,2.0,3.0,&r1,&r2);
+if(d<-1)
+{
+printf("a=0");
+}
+if(d==0)
+{
+printf("The roots are real and equal");
+}
+if(d=2)
+{
+printf("The roots are imaginary");
+}
+printf("%d",d);
+}
+
