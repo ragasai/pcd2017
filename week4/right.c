85e862abcb20f4f4d72f09b64efa786afebd9a97
Your Name <you@example.com>
2017-03-20 14:07:23 +0530

    right

---
+++ week4/right.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int right(int x,int n)
+{
+n=n%(4*8);
+int l=4*8-n;
+return x>>n|x<<l;
+}
+int main()
+{
+int n;
+int x;
+printf("Enter the value x and n = \n");
+scanf("%x%x",&n,&x);
+int y=right(1,7);
+printf("The rotated right number is = %x\n",y);
+}
+
